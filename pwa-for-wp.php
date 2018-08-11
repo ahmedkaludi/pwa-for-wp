0504ddab30898b3f6e532e9a7fb964102d3a7a30
Mohammed Kaludi <mohammed@magazine3.com>
2018-08-11 20:44:04 +0530

    Updated the tabbing and added ABSPATH check
    Updated the tabbing and added ABSPATH check

--- pwa-for-wp.php
+++ pwa-for-wp.php
@@ -7,25 +7,26 @@ Version: 1.0
 Author URI: https://ampforwp.com
 Text Domain: pwa-for-wp
 Domain Path: /languages/
- *
- * The main plugin file
- *
- */
-	define('PWAFORWP_PLUGIN_DIR', plugin_dir_path( __FILE__ ));
-	define('PWAFORWP_PLUGIN_URL', plugin_dir_url( __FILE__ ));
-	define('PWAFORWP_PLUGIN_VERSION', '1.0');
-        define('PWAFORWP_FILE_PREFIX', 'pwa');
-                
-        require_once PWAFORWP_PLUGIN_DIR."/admin/common-function.php";        
-        require_once PWAFORWP_PLUGIN_DIR."/service-work/serviceworker-class.php"; 
-        require_once PWAFORWP_PLUGIN_DIR."/service-work/file-creation-class.php"; 
-        require_once PWAFORWP_PLUGIN_DIR."/service-work/file_creation_init.php"; 
-              
-		if( pwaforwp_is_admin() ){
-			add_filter( 'plugin_action_links_' . plugin_basename(__FILE__),'pwaforwp_add_action_links');
-			require_once PWAFORWP_PLUGIN_DIR."admin/settings.php";
-		}
-                function pwaforwp_add_action_links($links){
-                        $mylinks = array('<a href="' . admin_url( 'admin.php?page=pwaforwp' ) . '">'.esc_html__( 'Settings', 'pwa-for-wp' ).'</a>');
-                        return array_merge( $links, $mylinks );
-                }
+*/
+
+// Exit if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) exit;
+
+define('PWAFORWP_PLUGIN_DIR', plugin_dir_path( __FILE__ ));
+define('PWAFORWP_PLUGIN_URL', plugin_dir_url( __FILE__ ));
+define('PWAFORWP_PLUGIN_VERSION', '1.0');
+define('PWAFORWP_FILE_PREFIX', 'pwa');
+        
+require_once PWAFORWP_PLUGIN_DIR."/admin/common-function.php";        
+require_once PWAFORWP_PLUGIN_DIR."/service-work/serviceworker-class.php"; 
+require_once PWAFORWP_PLUGIN_DIR."/service-work/file-creation-class.php"; 
+require_once PWAFORWP_PLUGIN_DIR."/service-work/file_creation_init.php"; 
+      
+if( pwaforwp_is_admin() ){
+	add_filter( 'plugin_action_links_' . plugin_basename(__FILE__),'pwaforwp_add_action_links');
+	require_once PWAFORWP_PLUGIN_DIR."admin/settings.php";
+}
+function pwaforwp_add_action_links($links){
+        $mylinks = array('<a href="' . admin_url( 'admin.php?page=pwaforwp' ) . '">'.esc_html__( 'Settings', 'pwa-for-wp' ).'</a>');
+        return array_merge( $links, $mylinks );
+}
