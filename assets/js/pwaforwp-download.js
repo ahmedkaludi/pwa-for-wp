661469d59b5db2decb1ab37adb4ee729611d72d2
Pradip <103096459+pradipshende@users.noreply.github.com>
2022-08-05 13:02:55 +0530

    Remove jQuery dependency in PWA plugin #743

--- assets/js/pwaforwp-download.js
+++ assets/js/pwaforwp-download.js
@@ -1,30 +1,29 @@
-jQuery(document).ready(function($) {
-	setTimeout(function() {
-		jQuery("a").on( "click", function(event) {
+(function(){
+	setTimeout(()=>{
+		document.addEventListener("click", function(event){
+			if(event.target.tagName === "A"){			
 			var isdownlod = event.target.attributes["download"] ? "1" : "0";
 			if(isdownlod == 1){
 				event.preventDefault();
-				console.log(isdownlod);
-				var url = jQuery(this).attr('href');
-				console.log("this: ",jQuery(this));
-				jQuery(this).removeAttr("href");
-				jQuery(this).attr("link", url);
+				var url = event.target.attributes["href"].value;
+				event.target.removeAttribute("href");
+				event.target.setAttribute("link",url);
 				var xhr = new XMLHttpRequest();
 			    xhr.open('GET', url, true);
 			    xhr.responseType = 'blob';
 			    xhr.onload = function(e) {
 			      if (this.status == 200) {
 			        var myBlob = this.response;
-			        console.log(myBlob);
 			        var link = document.createElement('a');
 			        link.href = window.URL.createObjectURL(myBlob);
 			        link.download = url;
 			        link.click();
-			        console.log("link",link); 
 			      }
 			    };
 			    xhr.send();
 			}
-		});
-	}, 1000);
-});
+			}
+		})
+
+	},1000)
+})()
--- assets/js/pwaforwp-video.js
+++ assets/js/pwaforwp-video.js
@@ -1,30 +1,34 @@
-jQuery(document).ready(function($) {
-	if(window.matchMedia('(display-mode: standalone)').matches) { 
-		console.log('Test online offline mode');
-		if (!navigator.onLine) {
-		  	console.log('Test offline');
-		  	jQuery('video').each(function(){
-				var src = this.src;
-				var ext = isVideo(src);
-				if(ext==true){
-					jQuery(this).attr('src','');
-					jQuery(this).parent().append('<p>MP4 video is not supported in service worker app</p>');
-				}
-			})
-		} 
-  	}
+(function(){
+	setTimeout(()=>{
+		if(window.matchMedia('(display-mode: standalone)').matches || window.matchMedia('(display-mode: fullscreen)').matches || window.matchMedia('(display-mode: minimal-ui)').matches) { 
+			if (!navigator.onLine) {
+				var video = document.getElementsByTagName("video");
+				Array.from(video).forEach(function(elm){
+					console.log("Each Loop",elm);
+					var src = elm.src;
+					console.log(src);
+					var ext = isVideo(src);
+					if(ext==true){
+						elm.setAttribute("src",'');
+						elm.parentElement.append('<p>MP4 video is not supported in service worker app</p>');
+					}
+				})
+	  		}
+		}
 
-  	function isVideo(filename) {
-	    var ext = getExtension(filename);
-	    switch (ext.toLowerCase()) {
-	    case 'mp4':
-	        return true;
-	    }
-	    return false;
-	}
+		function isVideo(filename) {
+		    var ext = getExtension(filename);
+		    switch (ext.toLowerCase()) {
+		    case 'mp4':
+		        return true;
+		    }
+		    return false;
+		}
 
-	function getExtension(filename) {
-	    var parts = filename.split('.');
-	    return parts[parts.length - 1];
-	}
-});
+		function getExtension(filename) {
+		    var parts = filename.split('.');
+		    return parts[parts.length - 1];
+		}
+
+	},1000)
+})()
