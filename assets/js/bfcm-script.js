fe5dc1c43363d986d75c228d2cd93ecad00514ea
Mohammed Qutubuddin Nasir <muhammed.qutubuddin786@gmail.com>
2025-11-28 18:24:35 +0530

    #983
    Improvement done prefix added to classes

--- assets/js/bfcm-script.js
+++ assets/js/bfcm-script.js
@@ -6,28 +6,28 @@ jQuery(document).ready(function($) {
 
     // 2. HTML Structure
     var bannerHTML = `
-        <div class="bfcm-banner-header" style="display:flex;">
-            <div class="bfcm-timer">
-                <div class="timer-box"><span id="d">00</span><label>Days</label></div>
-                <div class="timer-box"><span id="h">00</span><label>Hours</label></div>
-                <div class="timer-box"><span id="m">00</span><label>Mins</label></div>
-                <div class="timer-box"><span id="s">00</span><label>Secs</label></div>
+        <div class="pwaforwp-bfcm-banner-header" style="display:flex;">
+            <div class="pwaforwp-bfcm-timer">
+                <div class="pwaforwp-timer-box"><span id="d">00</span><label>Days</label></div>
+                <div class="pwaforwp-timer-box"><span id="h">00</span><label>Hours</label></div>
+                <div class="pwaforwp-timer-box"><span id="m">00</span><label>Mins</label></div>
+                <div class="pwaforwp-timer-box"><span id="s">00</span><label>Secs</label></div>
             </div>
-            <div class="bfcm-content">
+            <div class="pwaforwp-bfcm-content">
                 <h2>Black Friday / Cyber Monday / Christmas</h2>
                 <p>Sale is here! 50% off on Everything for a very limited time!!!</p>
             </div>
             <div>
-                <a href="${offerLink}" target="_blank" class="bfcm-btn">Claim Offer</a>
+                <a href="${offerLink}" target="_blank" class="pwaforwp-bfcm-btn">Claim Offer</a>
             </div>
         </div>
     `;
 
     // 3. Inject Banner
-    if($('.wrap > h1').length) { 
-        $('.wrap > h1').after(bannerHTML); 
+    if($('.pwaforwp-wrap > h1').length) { 
+        $('.pwaforwp-wrap > h1').after(bannerHTML); 
     } else { 
-        $('.wrap').prepend(bannerHTML); 
+        $('.pwaforwp-wrap').prepend(bannerHTML); 
     }
 
     // 4. Countdown Timer Logic
@@ -40,7 +40,7 @@ jQuery(document).ready(function($) {
         // Auto Hide logic (Client Side)
         if (distance < 0) {
             clearInterval(timerInterval);
-            $('.bfcm-banner-header').fadeOut(); 
+            $('.pwaforwp-bfcm-banner-header').fadeOut(); 
             return;
         }
 
@@ -49,9 +49,9 @@ jQuery(document).ready(function($) {
         var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
         var seconds = Math.floor((distance % (1000 * 60)) / 1000);
         
-        $('.bfcm-banner-header #d').text(days < 10 ? '0'+days : days);
-        $('.bfcm-banner-header #h').text(hours < 10 ? '0'+hours : hours);
-        $('.bfcm-banner-header #m').text(minutes < 10 ? '0'+minutes : minutes);
-        $('.bfcm-banner-header #s').text(seconds < 10 ? '0'+seconds : seconds);
+        $('.pwaforwp-bfcm-banner-header #d').text(days < 10 ? '0'+days : days);
+        $('.pwaforwp-bfcm-banner-header #h').text(hours < 10 ? '0'+hours : hours);
+        $('.pwaforwp-bfcm-banner-header #m').text(minutes < 10 ? '0'+minutes : minutes);
+        $('.pwaforwp-bfcm-banner-header #s').text(seconds < 10 ? '0'+seconds : seconds);
     }, 1000);
 });
